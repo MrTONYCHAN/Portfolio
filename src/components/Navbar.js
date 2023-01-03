6479617054311e66b5f2af966d8fb8fa9756f526
CHANDAN KUMAR <70132200+MrTONYCHAN@users.noreply.github.com>
2023-01-03 10:15:38 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -94,7 +94,7 @@ function NavBar() {
 
             <Nav.Item>
               <Nav.Link
-                href="https://soumyajitblogs.vercel.app/"
+                href="https://mrtonychan.vercel.app/"
                 target="_blank"
                 rel="noreferrer"
               >
@@ -104,7 +104,7 @@ function NavBar() {
 
             <Nav.Item className="fork-btn">
               <Button
-                href="https://github.com/soumyajit4419/Portfolio" // Change after the check-in
+                href="https://github.com/MrTONYCHAN/Portfolio" // Change after the check-in
                 target="_blank"
                 className="fork-btn-inner"
               >
